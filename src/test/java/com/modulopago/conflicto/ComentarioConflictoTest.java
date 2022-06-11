4e4a113e0c2d15a87a43990bb79d39ad533301dd
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2022-06-11 15:28:25 -0300

    error doble

--- src/test/java/com/modulopago/conflicto/ComentarioConflictoTest.java
+++ src/test/java/com/modulopago/conflicto/ComentarioConflictoTest.java
@@ -9,7 +9,7 @@ public class ComentarioConflictoTest {
     @Test
     public void testSetDescripcionConflictoVacia() {
         ComentarioConflicto comentarioConflicto = new ComentarioConflicto();
-        assertThrows(IllegalArgumentException.class, () -> comentarioConflicto.setDescripcionComentario(""));
+        assertThrows(IllegalArgumentException.class, () -> comentarioConflicto.setDescripcionComentario(123));
     }
 
     @Test
